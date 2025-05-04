a07808d709a134b23e482729929f9839ddcf7d6a
s3r3nd1pity <sasaserbinena@gmail.com>
2025-05-04 15:57:13 -0600

    main commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import {IUser} from "./models/IUser.ts";
 
 function App() {
-  return (
-    <></>
-  )
+    console.log("o");
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(value => value.json())
+            .then(value => setUsers(value));
+    }, []);
+
+
+    return (
+        <>
+            {
+                users.map(user => (
+                    <div>{user.name} </div>
+                ))
+            }
+        </>
+    )
 }
 
 export default App
---
+++ src/models/IUser.ts
@@ -0,0 +1,26 @@
+export interface IUser {
+	id: number;
+	name: string;
+	username: string;
+	email: string;
+	address: UserAddress;
+	phone: string;
+	website: string;
+	company: UserCompany;
+}
+export interface UserAddressGeo {
+	lat: string;
+	lng: string;
+}
+export interface UserAddress {
+	street: string;
+	suite: string;
+	city: string;
+	zipcode: string;
+	geo: UserAddressGeo;
+}
+export interface UserCompany {
+	name: string;
+	catchPhrase: string;
+	bs: string;
+}
