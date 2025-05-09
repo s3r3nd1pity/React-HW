87ac3ba09706fb093ff469eab55381ff440c761b
s3r3nd1pity <sasaserbinena@gmail.com>
2025-05-08 21:10:11 -0600

    main commit

---
+++ src/constants/urls.ts
@@ -0,0 +1,31 @@
+export const jsonPHBaseUrl = "https://jsonplaceholder.typicode.com";
+export const urlsPH = {
+    users: {
+        allUsers: jsonPHBaseUrl + "/users",
+        byId: (id: number) => jsonPHBaseUrl + "/users/" + id,
+    },
+    posts: {
+        allPosts: jsonPHBaseUrl + "/posts",
+        byId: (id: number) => jsonPHBaseUrl + "/posts/" + id,
+    },
+    todos: {
+        allTodos: jsonPHBaseUrl + "/todos",
+        byId: (id: number) => jsonPHBaseUrl + "/todos/" + id,
+    }
+}
+
+export const dummyJsonBaseUrl = "https://dummyjson.com";
+export const urlsDummy = {
+    users: {
+        allUsers: dummyJsonBaseUrl + "/users",
+        byId: (id: number) => dummyJsonBaseUrl + "/users/" + id,
+    },
+    posts: {
+        allPosts: dummyJsonBaseUrl + "/posts",
+        byId: (id: number) => dummyJsonBaseUrl + "/posts/" + id,
+    },
+    todos: {
+        allTodos: dummyJsonBaseUrl + "/todos",
+        byId: (id: number) => dummyJsonBaseUrl + "/todos/" + id,
+    }
+}
