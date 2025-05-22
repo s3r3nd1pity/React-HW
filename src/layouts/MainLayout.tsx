978ef4d9b3c6a468ba3127eea63bf86d6dd41aa5
s3r3nd1pity <sasaserbinena@gmail.com>
2025-05-22 15:32:31 -0600

    main commit

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,7 @@
-import Menu from "../components/menu/Menu.tsx";
-import {Outlet} from "react-router-dom";
-
 const MainLayout = () => {
     return (
         <div>
-            <Menu/>
-            <hr/>
-            <Outlet/>
+
         </div>
     );
 };
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,7 +2,5 @@ import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout.tsx";
 
 export const routes = createBrowserRouter([
-    {path:"/",element:<MainLayout/>, children:[
-            {}
-        ]}
+    {path: "/", element: <MainLayout/>}
 ])
